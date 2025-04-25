d3f5a0cab4be896bdd82868d1e009a74c3abc698
charmander <swistekxd@gmail.com>
2025-04-25 19:20:15 +0200

    1

--- consts/src/lib/endpoints/items.endpoints.ts
+++ consts/src/lib/endpoints/items.endpoints.ts
@@ -1,4 +1,6 @@
-import { basebackendUrl } from "@my-monorepo/consts"
+// import { basebackendUrl } from "@my-monorepo/consts"
+
+const basebackendUrl = "http://34.77.152.119"
 export const ItemsEndpoints = {
   getAll: (skip = 0, limit = 10) => `${basebackendUrl}/items?skip=${skip}&limit=${limit}`,
   getById: (id: number) => `${basebackendUrl}/items/${id}`,
