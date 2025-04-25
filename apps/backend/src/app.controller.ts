9f5adf51702e9b8fd249ca3aa0298e3705241a5a
charmander <swistekxd@gmail.com>
2025-04-25 17:16:13 +0200

    2

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -10,7 +10,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    console.log("aassssssssbbssa")
+    console.log("aasssssssssbbssa")
     return this.configService.get("dbconfig.dev.type")
   }
 }
