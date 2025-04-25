202020959479bb8eca4061f928a08a02f4288182
charmander <swistekxd@gmail.com>
2025-04-25 15:18:26 +0200

    run2

--- apps/backend/src/app.controller.ts
+++ apps/backend/src/app.controller.ts
@@ -1,19 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
+import { Controller, Get } from "@nestjs/common"
+import { AppService } from "./app.service"
+import { ConfigService } from "@nestjs/config"
 
 @Controller({
-  path: '',
+  path: "",
 })
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private configService: ConfigService
-  ) {}
+  constructor(private readonly appService: AppService, private configService: ConfigService) {}
 
   @Get()
   getHello(): string {
-    console.log('aaa');
-    return this.configService.get('dbconfig.dev.type');
+    console.log("aasa")
+    return this.configService.get("dbconfig.dev.type")
   }
 }
